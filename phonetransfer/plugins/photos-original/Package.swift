5dd0f77ef0ce665e8b7ea8402473ade64580b2a6
ojisan <ojisan@example.com>
2026-09-18 13:40:48 +0900

    phonetransfer ios: fix SPM platform syntax (.v14)

--- phonetransfer/plugins/photos-original/Package.swift
+++ phonetransfer/plugins/photos-original/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "CapacitorPluginPhotosOriginal",
-    platforms: [.iOS(.14)],
+    platforms: [.iOS(.v14)],
     products: [
         .library(
             name: "CapacitorPluginPhotosOriginal",
